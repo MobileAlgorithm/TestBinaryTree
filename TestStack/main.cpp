684110e957019357c282f5d6945df704a353f5b4
Lin <forrest.lin@sap.com>
2015-01-30 16:54:06 +0800

    Initial Commit

---
+++ TestStack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestStack
+//
+//  Created by Lin, Forrest on 1/30/15.
+//  Copyright (c) 2015 Lin, Forrest. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
